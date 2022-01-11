7f4dc5e8c4683c65610a54f6c6712ce91efc5c66
liwei <wei.li3@yintech.cn>
2022-01-11 15:10:23 +0800

    init

---
+++ lib/build/generated/source/buildConfig/debug/com/github/weiggle/drawable/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.github.weiggle.drawable;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.github.weiggle.drawable";
+  public static final String BUILD_TYPE = "debug";
+}
